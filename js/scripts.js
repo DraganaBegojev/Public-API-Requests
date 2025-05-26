9b729e906b51dece63a65147b70437083f2319f3
DraganaBegojev <163642152+DraganaBegojev@users.noreply.github.com>
2025-05-26 12:43:09 +0200

    Add employee card rendering logic
    Created displayEmployees() in scripts.js to dynamically render 12 employee cards
    using data from the Random User API.

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,21 @@ fetch('https://randomuser.me/api/?results=12&nat=us,gb,ca')
   .then(data => displayEmployees(data.results))
   .catch(error => console.error(error));
 
+function displayEmployees(employees) {
+    const gallery = document.querySelector('.gallery');
+
+    employees.forEach(employee => {
+        gallery.insertAdjacentHTML('beforeend', `
+            <div class="card">
+                <div class="card-img-container">
+                <img class="card-img" src="${employee.picture.large}" alt="profile picture">
+                </div>
+                <div class="card-info-container">
+                <h3 id="name" class="card-name cap">${employee.name.first} ${employee.name.last}</h3>
+                <p class="card-text">${employee.email}</p>
+                <p class="card-text cap">${employee.location.city}</p>
+                </div>
+            </div>
+        `);
+    });
+}
